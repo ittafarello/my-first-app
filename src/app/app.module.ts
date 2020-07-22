3d39833f6f85eaa4b9f971bce56ee7a7dc048288
ittafarello <itamartafarello@gmail.com>
2020-07-21 22:34:04 -0300

    New release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
